7d380c2844b53ad9d6562d028c7773a015e151fc
Jorge Acosta <jorgeacosta2004@icloud.com>
2026-09-11 23:49:39 -0500

    Initial Commit

---
+++ SaveMaxing/SaveMaxingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveMaxingApp.swift
+//  SaveMaxing
+//
+//  Created by Jorge Acosta on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SaveMaxingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
